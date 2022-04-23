946f70f7a893215030dbf0f0be2dedfeadd48ee0
Ivan Gorshkov <ivanagorshkov160@gmail.com>
2022-04-24 02:47:13 +0300

    exhibitions

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,12 @@
 const TerserPlugin = require("terser-webpack-plugin");
+const path = require('@babel/core/lib/vendor/import-meta-resolve');
 
 module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'react-bundle'),
+    filename: 'react-bundle.js',
+  },
   optimization: {
     minimize: true,
     minimizer: [new TerserPlugin()],
