f30456bc27e2e800175575447487d1680fcf9158
Ivan Gorshkov <ivanagorshkov160@gmail.com>
2022-04-24 03:32:55 +0300

    exhibitions

--- webpack.config.js
+++ webpack.config.js
@@ -1,75 +1,35 @@
-const path = require('path');
-const webpack = require('webpack');
+const TerserPlugin = require("terser-webpack-plugin");
+const path = require('@babel/core/lib/vendor/import-meta-resolve');
 
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: '[name].[contenthash:8].js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/'
+    filename: 'react-bundle.js',
   },
-  devtool: 'source-map',
   plugins: [
-    new WebpackPwaManifest({
-      name: 'KOYA',
-      short_name: 'KOYA',
-      description: 'Лучший сайт по поиску товаров',
-      icons: [
-        {
-          src: path.resolve('public/img/favicon.svg'),
-          sizes: [96, 128, 192, 256, 384, 512, 1024]
-        }
-      ],
-      start_url: '/',
-      display: 'fullscreen',
-      theme_color: '#d31e1e',
-      background_color: 'white'
-    }),
-    new HtmlWebpackPlugin({
-      template: './public/index.html'
-    }),
     new MiniCssExtractPlugin({
       filename: '[name].[contenthash:8].css'
     }),
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: 'public/img/',
-          to: 'img/'
-        },
-        {
-          from: 'public/all.js',
-          to: ''
-        },
-        {
-          from: 'public/img/svg',
-          to: 'img/svg'
-        },
-        {
-          from: 'public/sw.js',
-          to: ''
-        },
-        {
-          from: 'public/fonts/',
-          to: 'fonts/'
-        }
-      ]
-    })
-  ],
+    ],
   module: {
-    rules: [
-      {
-        test: /\.(js)$/,
-        use: 'babel-loader'
-      },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
+    rules : [
+      { test: /\.js?/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.(sass|css|scss)$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: "postcss-loader",
+            options: {
+              plugins: () => [
+                require("autoprefixer")()
+              ],
+            },
+          },
+          'sass-loader',
+        ]
       },
       {
         test: /.(s*)css$/,
@@ -80,35 +40,11 @@ module.exports = {
           'sass-loader'
         ]
       },
-      {
-        test: /\.svg$/,
-        use: {
-          loader: 'svg-url-loader',
-          options: {
-            name: './img/svg/[name].[ext]'
-          }
-        }
-      },
-      {
-        test: /\.png$/,
-        use: {
-          loader: 'file-loader',
-          options: {
-            name: './img/[name].[ext]'
-          }
-        }
-
-      },
-      {
-        test: /\.(ttf|woff|woff2)$/,
-        use: {
-          loader: 'file-loader',
-          options: {
-            name: './fonts/[name].[ext]'
-          }
-        }
-      }
     ]
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
   },
   mode: 'production'
 };
